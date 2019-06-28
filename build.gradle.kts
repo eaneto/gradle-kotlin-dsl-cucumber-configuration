c2f5950d6f4f573b7839f646f04c5716a7f1bcdc
Edison Neto <ednetoali@gmail.com>
2019-06-28 18:19:45 -0300

    Add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+import org.jetbrains.kotlin.com.google.common.collect.ImmutableList
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.31"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {}
+
+val cucumberRuntime by configurations.creating {
+    extendsFrom(configurations["testImplementation"])
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("io.cucumber:cucumber-java:4.3.1")
+    testImplementation("io.cucumber:cucumber-junit:4.3.1")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "11"
+    }
+}
+
+task("cucumber") {
+    dependsOn("assemble", "compileTestJava")
+    doLast {
+        javaexec {
+            main = "cucumber.api.cli.Main"
+            classpath = cucumberRuntime + sourceSets.main.get().output + sourceSets.test.get().output
+            args = listOf("--plugin", "pretty", "--glue", "gradle.cucumber", "src/test/resources")
+        }
+    }
+}
